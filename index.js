172d9cc048577828a6369e75959f1bd3b5e5624c
Carlos Macias <cimm21187@gmail.com>
2023-09-07 23:58:39 -0400

    Created array of questions for user input.

--- index.js
+++ index.js
@@ -4,7 +4,55 @@ const fs = require('fs');
 const generateMarkdown = require('./assets/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of your project:'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'How do users install your project?'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do users use your project once it is installed?'
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute to your project?'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'How can users test your project?'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address?'
+      }
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
