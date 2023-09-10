362b5d62b6ee02ab5df35af9718c72fad66c1cd7
Carlos Macias <cimm21187@gmail.com>
2023-09-09 21:35:07 -0400

    Created function for the license badge.

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'Apache 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'GPLv3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'BSD 3-Clause':
+      return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
