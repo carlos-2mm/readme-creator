a5415c97af2a78af78f765b847ed1a51e9f208b6
Carlos Macias <cimm21187@gmail.com>
2023-09-10 00:56:24 -0400

    Created function to generate markdown for README.

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
@@ -15,7 +15,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch(license) {
@@ -34,13 +34,84 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None' || !license) {
+    return '';
+  }
+  return `## License
+
+This project is licensed under the ${license} license.
+
+For more information see [this link](${renderLicenseLink(license)}).
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+---
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contribution)
+* [Credits](#credits)
+* [Features](#features)
+* [Test](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+---
+
+## Installation
+
+${data.installation}
+
+---
+
+## Usage
+
+${data.usage}
+
+---
+
+## Contribution
+
+${data.contribution}
+
+---
+
+## Credits
+${data.credits}
+
+---
+
+## Tests
+
+${data.test}
+
+---
+
+${renderLicenseSection(data.license)}
+
+---
+
+## Questions
+
+- GitHub: Please visit my GitHub profile:
+[${data.github}](https://github.com/${data.github})
+- Email: For clarifications or questions about this project, please send an email to:
+ ${data.email}
 `;
 }
+
 
 module.exports = generateMarkdown;
