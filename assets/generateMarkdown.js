9da47beafbe3a4ae78a183acece2dabf84ab1e80
Carlos Macias <cimm21187@gmail.com>
2023-09-09 23:44:51 -0400

    Created function for the license link.

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -17,7 +17,20 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
